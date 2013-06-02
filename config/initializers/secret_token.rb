f950677679c6bf6f3594646e7175e74e70b3e2b5
James Lin <jameslin101@gmail.com>
2013-06-02 19:58:05 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fundlocal::Application.config.secret_token = '61fe8f0c995f52746a44749ae5d3121f63b57083c6872d9cd51fd18e8aeb74b988d416d24358def7bf5d49958085bcd2aa09caafc565aaf14ecd547cb36bc1ef'
