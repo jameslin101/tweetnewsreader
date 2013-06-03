1202bb69820373117598c8f45f5e3d9d8099d489
James Lin <jameslin101@gmail.com>
2013-06-02 20:08:57 -0400

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Fundlocal::Application.routes.draw do
 
-  devise_for :users
-
   root :to => "pages#home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130603000609) do
+
+  create_table "widgets", :force => true do |t|
+    t.string   "name"
+    t.string   "purpose"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
